12b01b45257e69033e33cf3a8e83ea1ac8a0fe61
Rohan Mogali <141552712+RohannMogalii@users.noreply.github.com>
2024-04-22 10:15:24 +0530

    Add files via upload

---
+++ Hackerank/Basic_Select/Weather Observation Station 10.sql
@@ -0,0 +1,9 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+
+select DISTINCT CITY
+from STATION
+where (CITY NOT LIKE '%a' and
+       CITY NOT LIKE '%e' and
+       CITY NOT LIKE '%i' and
+       CITY NOT LIKE '%o' and
+       CITY NOT LIKE '%u');
